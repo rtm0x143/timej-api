6acd4e4d818e8c15eaa924090fd8e51d76481fe0
rtm0x143 <russia-artyom@mail.ru>
2023-02-23 01:38:58 +0700

    feat (data) : connection string thew env
    Added 'Production' configuration of data base. Now app extracts connection string from 'TIMEJ_DB_CONN' environment variable.

---
+++ Helpers/WebAppBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace TimejApi.Helpers
+{
+    public static class WebAppBuilderExtensions
+    {
+        public static string GetConnectionString(this WebApplicationBuilder builder)
+        {
+            builder.Configuration.AddEnvironmentVariables();
+            var connection = builder.Environment.IsDevelopment()
+                ? builder.Configuration.GetConnectionString("Default")
+                : builder.Configuration.GetValue<string>("TIMEJ_DB_CONN");
+
+            if (connection is not string)
+                throw new ArgumentException("Failed to extract connection string from configuration;" +
+                    "Expected [ConnectionStrings:Default] prop or 'TIMEJ_DB_CONN' env variable.");
+
+            return connection;
+        }
+    }
+}
