7e346ba69d6506bf66b6340721b8f1534b12d8cd
rtm0x143 <russia-artyom@mail.ru>
2023-02-22 21:53:25 +0700

    Added empty ScheduleController representing route "/api/shedule"

--- Controllers/ScheduleController.cs
+++ Controllers/ScheduleController.cs
@@ -1,25 +1,56 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Routing;
-using TimejApi.Data;
-using TimejApi.Data.Models;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.CodeAnalysis;
 
 namespace TimejApi.Controllers
 {
     [ApiController]
-    [Route("[Controller]")]
+    [Route("api/schedule")]
     public class ScheduleController : Controller
     {
-        private readonly ScheduleDbContext _context;
+        public record LessonGroup(uint? SubGroupNumber, uint GroupNumber);
+        public record Teacher(Guid Id, string Fullname);
 
-        public ScheduleController(ScheduleDbContext context)
+        public record Lesson(Guid Id, DateOnly Date, uint LessonNumber, string LessonType, 
+            string Subject, LessonGroup[] Groups, Teacher Teacher);
+
+        public record ScheduleDay(DateOnly Date, Lesson[] Lessons);
+
+        [HttpGet]
+        public async Task<ActionResult<ScheduleDay[]>> Get(
+            [FromQuery] DateOnly beginDate,
+            [FromQuery] DateOnly endDate,
+            [FromQuery] uint? groupNumber, 
+            [FromQuery] Guid? teacherId, 
+            [FromQuery] [NotNullIfNotNull(nameof(buildingNumber))] uint? auditoryNumber,
+            [FromQuery] [NotNullIfNotNull(nameof(auditoryNumber))] uint? buildingNumber,
+            [FromQuery] bool isOnline = false) 
         {
-            _context = context;
+            throw new NotImplementedException();
         }
-        [HttpPost("addUser")]
-        public IActionResult AddUser(User user)
+
+        [HttpGet("default")]
+        [Authorize]
+        public async Task<ActionResult<ScheduleDay[]>> GetDefault()
         {
-            _context.Users.Add(user);
-            return Ok();
+            throw new NotImplementedException();
+        }
+
+        public record LessonCreation(DateOnly Date, uint LessonNumber, Guid LessonId,
+            Guid SubjectId, LessonGroup[] Groups, Guid TeacherId);
+
+        [HttpPost]
+        // TODO: Add policy [Authorize(Policy = "SheduleEditor")]
+        public async Task<ActionResult<Lesson>> Post(LessonCreation lesson)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        // TODO: Add policy [Authorize(Policy = "SheduleEditor")]
+        public async Task<ActionResult<Lesson>> Rut(LessonCreation lesson)
+        {
+            throw new NotImplementedException();
         }
     }
 }
