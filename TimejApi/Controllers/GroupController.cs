228bfe71822c5395dd834357ce96a242aa0d1d5f
rtm0x143 <eartyom323@gmail.com>
2023-02-28 19:38:45 +0700

    feat (group_route) : desigend endpoints
    - Created empty GroupController
    
    ISSUES CLOSED: TJ-60

---
+++ TimejApi/Controllers/GroupController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TimejApi.Data.Dtos;
+using UserRole = TimejApi.Data.Entities.User.Role;
+
+namespace TimejApi.Controllers
+{
+    [Route("api/faculty/{facultyId}/group")]
+    [ApiController]
+    public class GroupController : ControllerBase
+    {
+        public GroupController()
+        {
+        }
+
+        /// <summary>
+        /// Get all groups related to that Faculty
+        /// </summary>
+        [HttpGet]
+        public Task<Action<GroupDto[]>> GetAll(Guid facultyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Get concrete Group
+        /// </summary>
+        [HttpGet("{id}")]
+        public Task<Action<GroupDto>> Get(Guid facultyId, Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Creates new Group 
+        /// </summary>
+        /// <remarks>
+        /// Requires MODERATOR role 
+        /// </remarks>
+        [HttpPost]
+        [Authorize(Roles = nameof(UserRole.MODERATOR))]
+        public Task<Action<GroupDto>> Post(Guid facultyId, GroupCreaton group)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Updates some Group 
+        /// </summary>
+        /// <remarks>
+        /// Requires MODERATOR role 
+        /// </remarks>
+        [HttpPut("{id}")]
+        [Authorize(Roles = nameof(UserRole.MODERATOR))]
+        public Task<Action<GroupDto>> Put(Guid facultyId, Guid id, GroupCreaton group)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
