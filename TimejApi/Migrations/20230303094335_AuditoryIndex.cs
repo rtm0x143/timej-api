530f74a65def870a15bbdb25881cbbf0e306b42d
rtm0x143 <eartyom323@gmail.com>
2023-03-03 16:44:57 +0700

    feat(auditory_route) : implemented controller
    - redesigned route to use Ids
    - implemented AuditoryController
    - added unique Building + AuditoryNumber index
    
    ISSUES CLOSED: TJ-76

---
+++ TimejApi/Migrations/20230303094335_AuditoryIndex.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TimejApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AuditoryIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Auditories_AuditoryNumber_BuildingId",
+                table: "Auditories",
+                columns: new[] { "AuditoryNumber", "BuildingId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Auditories_AuditoryNumber_BuildingId",
+                table: "Auditories");
+        }
+    }
+}
