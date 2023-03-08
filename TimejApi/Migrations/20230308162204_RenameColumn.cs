55b226396de853e46a790b387780298598541c3b
Slava Chaunin <slava.chaunin+puj@gmail.com>
2023-03-08 23:29:24 +0700

    fix (map) : renamed attribute

---
+++ TimejApi/Migrations/20230308162204_RenameColumn.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TimejApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Number",
+                table: "Lessons",
+                newName: "LessonNumber");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Lessons_Date_Number_TeacherId",
+                table: "Lessons",
+                newName: "IX_Lessons_Date_LessonNumber_TeacherId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Lessons_Date_Number_AuditoryId",
+                table: "Lessons",
+                newName: "IX_Lessons_Date_LessonNumber_AuditoryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LessonNumber",
+                table: "Lessons",
+                newName: "Number");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Lessons_Date_LessonNumber_TeacherId",
+                table: "Lessons",
+                newName: "IX_Lessons_Date_Number_TeacherId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Lessons_Date_LessonNumber_AuditoryId",
+                table: "Lessons",
+                newName: "IX_Lessons_Date_Number_AuditoryId");
+        }
+    }
+}
