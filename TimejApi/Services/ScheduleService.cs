11d1fa8f9b13df8bff3915b04c2faaa5ec2025f9
Slava Chaunin <slava.chaunin+puj@gmail.com>
2023-03-10 20:40:47 +0700

    feat :lesson creation

--- TimejApi/Services/ScheduleService.cs
+++ TimejApi/Services/ScheduleService.cs
@@ -1,6 +1,7 @@
 ﻿using Mapster;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using TimejApi.Data;
 using TimejApi.Data.Dtos;
 using TimejApi.Data.Entities;
@@ -53,9 +54,19 @@ namespace TimejApi.Services
             return result.ToArray();
         }
 
-        private async Task CheckCollisions(DateOnly beginDate, DateOnly endDate, Guid? groupNumber, Guid? teacherId, Guid? buildingId, uint? auditoryNumber, bool isOnline)
+        private async Task<bool> CheckCollisions(LessonCreation lesson)
         {
-
+            var isOnline = lesson.AuditoryId is null;
+            var query = new LessonQuerry(_dbContext.Lessons);
+            var lessonMoment = query.SpecifyDate(lesson.Date, lesson.Date).SpecifyTimeSlot(lesson.LessonNumber);
+            var teacherBusy = !(await lessonMoment.SpecifyTeacher(lesson.TeacherId).Get().ToArrayAsync()).IsNullOrEmpty();
+            var auditoryBusy = !(isOnline ? null : await lessonMoment.SpecifyPlace(lesson.AuditoryId, false).Get().ToArrayAsync()).IsNullOrEmpty();
+            var groupsAnyBusy = !(await lessonMoment.SpecifyGroupsAny(lesson.AttendingGroups.Select(x => x.GroupId).ToArray()).Get().ToArrayAsync()).IsNullOrEmpty();
+            _logger.LogInformation($"Collisions for lesson {lesson.Date}, timeslot: {lesson.LessonNumber}\n" +
+                $" teacher {lesson.TeacherId} is busy: {teacherBusy} \n" +
+                $" auditory {lesson.AuditoryId} is busy: {auditoryBusy}\n" +
+                $" some groups are busy: {groupsAnyBusy}\n");
+            return teacherBusy && auditoryBusy && groupsAnyBusy;
         }
 
         public async Task DeleteLessons(Guid replicaId)
@@ -92,22 +103,33 @@ namespace TimejApi.Services
                 throw new ArgumentException("The lesson must be within bounds you want to create it in");
             }
 
+
+            async Task<Lesson> ProcessLesson(DateOnly day)
+            {
+                var isCollided = await CheckCollisions(lesson);
+                if (isCollided)
+                {
+                    throw new ArgumentException($"The lesson you tried to add on {lesson.Date} in {lesson.LessonNumber} slot had collisions");
+                }
+                var newLesson = lesson.Adapt<Lesson>();
+                newLesson.Date = day;
+                return newLesson;
+            }
+
+
             var lessonsRange = new List<Lesson>();
+            for (DateOnly day = lesson.Date.AddDays(-DEFAULT_INTERVAL_DAYS); day >= beginDate; day = day.AddDays(-DEFAULT_INTERVAL_DAYS))
+            {
+                lessonsRange.Add(await ProcessLesson(day));
+            }
             for (DateOnly day = lesson.Date; day <= endDate; day = day.AddDays(DEFAULT_INTERVAL_DAYS))
             {
-                var new_lesson = lesson.Adapt<Lesson>();
-                new_lesson.Date = day;
-                lessonsRange.Add(new_lesson);
+                lessonsRange.Add(await ProcessLesson(day));
             }
-            for (DateOnly day = (DateOnly)beginDate; day < lesson.Date; day = day.AddDays(DEFAULT_INTERVAL_DAYS))
-            {
-                var new_lesson = lesson.Adapt<Lesson>();
-                new_lesson.Date = day;
-                lessonsRange.Add(new_lesson);
-            }
+
+
             await _dbContext.Lessons.AddRangeAsync(lessonsRange.Select(x => { x.ReplicaId = Guid.NewGuid(); return x; }));
             await _dbContext.SaveChangesAsync();
-            Console.WriteLine(lessonsRange[0]);
             return lessonsRange.Select(x => x.Adapt<LessonDto>()).ToArray();
         }
 
@@ -155,6 +177,13 @@ namespace TimejApi.Services
             _lessons = _lessons.Where(x => x.Date >= beginDate && x.Date <= endDate);
             return this;
         }
+
+        public LessonQuerry SpecifyTimeSlot(uint timeSlot)
+        {
+            LessonNumber slotEnum = (LessonNumber)Enum.ToObject(typeof(LessonNumber), timeSlot);
+            _lessons = _lessons.Where(x => x.LessonNumber == slotEnum);
+            return this;
+        }
         public LessonQuerry SpecifyTeacher(Guid? teacherId)
         {
             if (teacherId is null) return this;
@@ -166,6 +195,12 @@ namespace TimejApi.Services
         {
             if (groupId is null) return this;
             _lessons = _lessons.Where(x => x.AttendingGroups.Select(x => x.GroupId).Contains((Guid)groupId)).Include(x => x.AttendingGroups);
+            return this;
+        }
+
+        public LessonQuerry SpecifyGroupsAny(Guid[] groupIds)
+        {
+            _lessons = _lessons.Where(x => x.AttendingGroups.Any(x => groupIds.Contains(x.GroupId)));
             return this;
         }
 
