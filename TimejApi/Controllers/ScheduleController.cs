dc7b337793e65bb481b18e28d10931dc1f009f1e
rtm0x143 <eartyom323@gmail.com>
2023-03-12 22:56:40 +0700

    fix (schedule) : fixed check

--- TimejApi/Controllers/ScheduleController.cs
+++ TimejApi/Controllers/ScheduleController.cs
@@ -69,7 +69,7 @@ namespace TimejApi.Controllers
                 return Problem(statusCode: StatusCodes.Status403Forbidden,
                                detail: "You don't have enough permissions to perform this action");
             }
-            if (await _userService.IsTeacher(lesson.TeacherId))
+            if (!await _userService.IsTeacher(lesson.TeacherId))
             {
                 return BadRequest($"Teacher with id {lesson.TeacherId} is invalid");
             }
